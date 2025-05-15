5e1dc5ff9a2324310af1ee39938809c54413c01e
yayazi02 <yinyin719@outlook.com>
2025-05-16 04:54:39 +0800

    bib.js

--- data/generated/bib.js
+++ data/generated/bib.js
@@ -1,17 +1,82 @@
-﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
-        "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
-    }
+const generatedBibEntries = {
+  "Bartlette2006": {
+    "title": "Effect of network latency on interactive musical performance",
+    "author": "Bartlette, C.; Headlam, D.; Bocko, M.; Velikic, G.",
+    "journal": "Music Perception",
+    "volume": "24",
+    "number": "1",
+    "pages": "49-62",
+    "year": "2006",
+    "publisher": "University of California Press",
+    "doi": "10.1525/mp.2006.24.1.49"
+  },
+  "Jack2016": {
+    "title": "Effect of latency on performer interaction and subjective quality assessment of a digital musical instrument",
+    "author": "Jack, R.H.; Stockman, T.; McPherson, A.P.",
+    "journal": "Audio Mostly 2016",
+    "pages": "116-123",
+    "year": "2016",
+    "publisher": "ACM",
+    "doi": "10.1145/2986416.2986420"
+  },
+  "Turchet2023": {
+    "title": "Immersive networked music performance systems: identifying latency factors",
+    "author": "Turchet, L.; Tomasetti, M.",
+    "journal": "3rd International Conference on Immersive and 3D Audio (I3DA)",
+    "year": "2023",
+    "publisher": "IOS Press",
+    "url": "https://ebooks.iospress.nl/volumearticle/22374"
+  },
+  "Carot2024": {
+    "title": "Low-latency real-time network music performance systems: A review",
+    "author": "Car\u00f4t, A.; Werner, C.; Br\u00fcnger, N.",
+    "journal": "IEEE Access",
+    "year": "2024",
+    "publisher": "IEEE",
+    "url": "https://ieeexplore.ieee.org/document/10543134"
+  },
+  "Grimm2024": {
+    "title": "Interactive low delay music and speech communication via network connections (OVBOX)",
+    "author": "Grimm, G.",
+    "journal": "Acta Acustica",
+    "volume": "8",
+    "pages": "1-7",
+    "year": "2024",
+    "doi": "10.1051/aacus/2024005"
+  },
+  "Yamaha2021": {
+    "title": "SYNCROOM \u2013 A Collaborative Platform for Online Music Performance",
+    "author": "Yamaha Corporation",
+    "year": "2021",
+    "journal": "Yamaha Developer",
+    "url": "https://syncroom.yamaha.com/global/"
+  },
+  "FarPlay2022": {
+    "title": "FarPlay: Play Music Online Like Never Before",
+    "author": "FarPlay",
+    "year": "2022",
+    "journal": "FarPlay.io",
+    "url": "https://farplay.io/"
+  },
+  "Pond2022": {
+    "title": "The Speed of Sound: Solving the Latency for Distant Musical Collaboration",
+    "author": "Pond, W.",
+    "year": "2022",
+    "journal": "Arizona State University",
+    "url": "https://tech.asu.edu/features/speed-sound"
+  },
+  "NTT2022": {
+    "title": "Remote Chorus Demonstration for Ultra-Low Latency Real-Time Video Communication",
+    "author": "NTT Corporation",
+    "year": "2022",
+    "journal": "NTT News Release",
+    "url": "https://group.ntt/en/newsrelease/2022/12/01/221201a.html"
+  },
+  "Elk2020": {
+    "title": "Using the Internet to Let the Bands Play On",
+    "author": "Elk Audio",
+    "year": "2020",
+    "journal": "Axios",
+    "url": "https://www.axios.com/2020/08/11/5g-music-wifi-aloha-elk-latency-remote-bands"
+  }
 };
